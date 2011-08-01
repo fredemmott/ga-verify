c64fe79d47a6d07e42226515af376531b5ec5edb
Fred Emmott <mail@fredemmott.co.uk>
2011-08-01 08:12:14 +0100

    Security improvements.
    - max one attempt per user per 15s
    - no re-use of a token within 10 minutes (and it only allows +- 1 token)
    
    Note the one attempt per 15s == DoS possibility, so combine with some
    other form of authentication.

--- lib/ga_verify/handler.rb
+++ lib/ga_verify/handler.rb
@@ -5,6 +5,11 @@ require 'rotp'
 
 module GAVerify
   class Handler
+    def initialize
+      @last_seen   = Hash.new(0)
+      @used_tokens = Hash.new{Hash.new(0)}
+    end
+
     def check_user user, token
       unless user =~ /^[a-z]+$/
         return GAVerify::Result::BAD_USER
@@ -16,18 +21,28 @@ module GAVerify
       unless File.exists? path
         return GAVerify::Result::NO_GOOGLE_AUTH
       end
+      now = Time.now.to_i
+      # Max one login every 15s
+      if @last_seen[user] >= now - 15
+        return GAVerify::Result::BAD_TOKEN
+      end
+      @last_seen[user] = now
 
       secret = File.open(path, 'r').first.strip
       totp = ROTP::TOTP.new(secret)
 
-      now = Time.now.to_i
-      times = [now - 60, now, now + 60]
-
+      # Allow +- 1 token
+      times = [now - 30, now, now + 30]
       if times.any?{|time| totp.verify(token, time)}
-        return GAVerify::Result::SUCCESS
-      else
-        return GAVerify::Result::BAD_TOKEN
+        # disallow token re-use within 10 minutes
+        if @used_tokens[user][token] < now - 600
+          @used_tokens[user][token] = now
+          # Cleanup
+          @used_tokens[user].reject!{|k,v| used_tokens[user][token] < now - 600}
+          return GAVerify::Result::SUCCESS
+        end
       end
+      return GAVerify::Result::BAD_TOKEN
     end
   end
 end
